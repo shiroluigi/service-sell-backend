e30bc90d7d0a5c923f5c8dd83566a776c499db97
Rohit Mondal <rohit.luiji3@gmail.com>
2026-01-21 14:40:05 +0000

    added email services in different classes managed by properties file

---
+++ src/main/java/ecommerce/ecom/common/SMTPservice.java
@@ -0,0 +1,31 @@
+package ecommerce.ecom.common;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@ConditionalOnProperty(
+    value = "email.provider",
+    havingValue = "smtp"
+)
+public class SMTPservice implements EmailService {
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    @Override
+    public void sendEmail(String to, String subject, String body) {
+        try {
+            SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+            simpleMailMessage.setTo(to);
+            simpleMailMessage.setSubject(subject);
+            simpleMailMessage.setText(body);
+
+            javaMailSender.send(simpleMailMessage);
+        } catch (Exception e) {
+            System.out.println("Exception occured! " + e.getMessage());
+        }
+    }
+}
