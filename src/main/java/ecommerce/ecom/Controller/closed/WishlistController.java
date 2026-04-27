129f6a2a6826bd9c7d1c205b13fae6026555683c
Rohit Mondal <rohit.luiji3@gmail.com>
2026-04-27 10:49:58 +0000

    remove from wishlist

--- src/main/java/ecommerce/ecom/Controller/closed/WishlistController.java
+++ src/main/java/ecommerce/ecom/Controller/closed/WishlistController.java
@@ -2,6 +2,7 @@ package ecommerce.ecom.Controller.closed;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -27,5 +28,10 @@ public class WishlistController {
     @GetMapping("/all")
     public ResponseEntity<?> getAllWishlist(@RequestParam String userId){
         return wishlistService.getAllWishlist(userId);
+    }
+
+    @DeleteMapping("/remove")
+    public ResponseEntity<?> removeWish(@RequestParam String wishId){
+        return wishlistService.removeWish(wishId);
     }
 }
--- src/main/java/ecommerce/ecom/Service/WishlistService.java
+++ src/main/java/ecommerce/ecom/Service/WishlistService.java
@@ -40,27 +40,29 @@ public class WishlistService {
             userDto.setId(wishDto.getUser_id());
             userDto.setEmail(wishDto.getEmail());
             List<User> userList = userCommonService.findUser(userDto);
-            if (serviceOptional.isPresent() && !userList.isEmpty()){
+            if (serviceOptional.isPresent() && !userList.isEmpty()) {
                 Wishlist wishlist = new Wishlist();
                 wishlist.setService(serviceOptional.get());
                 wishlist.setUser(userList.get(0));
                 wishlist.setTimestamp(LocalDateTime.now());
                 wishlistRepository.save(wishlist);
-                return new ResponseEntity<>(new CommonDTO("ADD WISHLIST","CREATED",""),HttpStatus.CREATED);
-            }else{
-                return new ResponseEntity<>(new CommonDTO("ADD WISHLIST","ERROR","User or service not found"),HttpStatus.NOT_FOUND);
+                return new ResponseEntity<>(new CommonDTO("ADD WISHLIST", "CREATED", ""), HttpStatus.CREATED);
+            } else {
+                return new ResponseEntity<>(new CommonDTO("ADD WISHLIST", "ERROR", "User or service not found"),
+                        HttpStatus.NOT_FOUND);
             }
         } catch (Exception e) {
-            return new ResponseEntity<>(new CommonDTO("ADD WISHLIST","ERROR","Something went wrong.."),HttpStatus.INTERNAL_SERVER_ERROR);
-        }   
+            return new ResponseEntity<>(new CommonDTO("ADD WISHLIST", "ERROR", "Something went wrong.."),
+                    HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     public ResponseEntity<?> getAllWishlist(String userId) {
         try {
             List<Wishlist> wl_raw = wishlistRepository.findByUserId(userId);
             List<WishlistDTO> wishlistDto = new ArrayList<>();
-            if (!wl_raw.isEmpty()){
-                for(Wishlist wl : wl_raw){
+            if (!wl_raw.isEmpty()) {
+                for (Wishlist wl : wl_raw) {
                     WishlistDTO wldto = new WishlistDTO();
                     wldto.setId(wl.getId());
                     wldto.setService_id(String.valueOf(wl.getService().getId()));
@@ -70,12 +72,37 @@ public class WishlistService {
                     wldto.setUser_name(wl.getUser().getFirstName() + " " + wl.getUser().getLastName());
                     wishlistDto.add(wldto);
                 }
-                return new ResponseEntity<>(wishlistDto,HttpStatus.OK);
-            }else{
-                return new ResponseEntity<>(new CommonDTO("GET WISHLIST","ERROR","No items found"),HttpStatus.NOT_FOUND);
+                return new ResponseEntity<>(wishlistDto, HttpStatus.OK);
+            } else {
+                return new ResponseEntity<>(new CommonDTO("GET WISHLIST", "ERROR", "No items found"),
+                        HttpStatus.NOT_FOUND);
             }
         } catch (Exception e) {
-            return new ResponseEntity<>(new CommonDTO("GET WISHLIST","ERROR","Something went wrong.."),HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(new CommonDTO("GET WISHLIST", "ERROR", "Something went wrong.."),
+                    HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public ResponseEntity<?> removeWish(String wishId) {
+        try {
+            Optional<Wishlist> wlItem = wishlistRepository.findById(wishId);
+
+            if (wlItem.isEmpty()) {
+                return ResponseEntity
+                        .status(404)
+                        .body("Wishlist item not found");
+            }
+
+            wishlistRepository.delete(wlItem.get());
+
+            return ResponseEntity
+                    .status(200)
+                    .body("Removed from wishlist successfully");
+
+        } catch (Exception e) {
+            return ResponseEntity
+                    .status(500)
+                    .body("Something went wrong");
         }
     }
 }
